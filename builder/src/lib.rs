8f9a2dcec7355919e198c41bfd23f3d6464dac1f
Suprakon Cheepsamut <scheepsamut@sureco.com>
2025-11-13 23:40:41 +0700

    complete builder 01

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,8 +1,11 @@
 use proc_macro::TokenStream;
-
+use syn::{self, DeriveInput, parse_macro_input};
+use quote::quote;
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
+    let input = parse_macro_input!(input as DeriveInput);
 
-    unimplemented!()
+    let expaned = quote!{};
+
+    TokenStream::from(expaned)
 }
